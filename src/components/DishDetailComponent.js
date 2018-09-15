9202db6f24ae112a36634102fb632c7e62671d7f
harshendrashah <harshendrashah812@gmail.com>
2018-09-15 22:34:28 +0530

    updates

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -7,28 +7,57 @@ class DishDetail extends Component {
         super(props);
 
     }
-     
+
+    
     renderDish(dish) {
         if (dish!=null){
-            
+            return(
+                <Card className="clo=12 col-md-5 m-1">
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle><strong>{dish.name}</strong></CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+                
+            );
         } else {
-            <div></div>
+            return(
+                <div></div>
+            );
         }
     }
 
-    renderComments() {
-
+    renderComments(array) {
+        if(array.length != 0) {
+            return array.map(comment => (
+                <div className="list-unstyled">
+                    <div>
+                        {comment.comment}
+                    </div>
+                    <div>
+                        {comment.author} {comment.date}
+                    </div>
+                </div>
+            ));
+        } else {
+            return(
+                <div></div>
+            );
+        }
     }
 
     render() {
         return(
-            <Card className="col-12 col-md-5">
-                <CardImg width="100%" src={this.props.selectedDish.image} alt={this.props.selectedDish.name} />
-                <CardBody>
-                    <CardTitle><strong>{this.props.selectedDish.name}</strong></CardTitle>
-                    <CardText>{this.props.selectedDish.description}</CardText>
-                </CardBody>
-            </Card> 
+            <div className="row">
+                
+                    {this.renderDish(this.props.selectedDish)}
+                
+                <div className="col-12 col-md-5 m-1">
+                    <h4>Comments</h4>
+                    {this.renderComments(this.props.selectedDish.comments)}
+                </div> 
+            </div>
         );
     }
 }
